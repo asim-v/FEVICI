df2aebac97a0b4a97757c93f0b1a074e431e3daf
Emanuell Paredes <emanuellp@hotmail.com>
2020-06-01 15:12:47 -0500

    enable migrations, test register user firebase, enable session

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 ## export FLASK_APP=application.py
 ## export FLASK_ENV=development
-from flask import Flask, render_template, request, session, send_file, send_from_directory, safe_join, abort
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
 from flask_session import Session
-import datetime
 from flask_firebase import FirebaseAuth
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
@@ -21,23 +19,31 @@ import os
 
 #Sessions, init flask
 app = Flask(__name__)
-Session(app)
+app.secret_key = 'secret_8473738'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/fevici.db'
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config['FIREBASE_API_KEY'] = ''
 app.config['FIREBASE_PROJECT_ID'] = ''
 app.config['FIREBASE_AUTH_SIGN_IN_OPTIONS'] = 'email'
 
+
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+db.create_all()
 auth = FirebaseAuth(app)
 login_manager = LoginManager()
 
 login_manager.init_app(app)
-
+sess = Session()
 app.register_blueprint(auth.blueprint, url_prefix='/auth')
+#Session(app)
 
+sess.init_app(app)
 
-class Account(UserMixin, db.Model):
-    __tablename__ = 'accounts'
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     account_id = db.Column(db.Integer, primary_key=True)
     firebase_user_id = db.Column(db.Text, unique=True)
     email = db.Column(db.Text, unique=True, nullable=False)
@@ -45,12 +51,15 @@ class Account(UserMixin, db.Model):
     name = db.Column(db.Text)
     photo_url = db.Column(db.Text)
 
+    def get_id(self):
+        return (self.account_id)
+
 
 @auth.production_loader
 def production_sign_in(token):
-    account = Account.query.filter_by(firebase_user_id=token['sub']).one_or_none()
+    account = User.query.filter_by(firebase_user_id=token['sub']).one_or_none()
     if account is None:
-        account = Account(firebase_user_id=token['sub'])
+        account = User(firebase_user_id=token['sub'])
         db.session.add(account)
     account.email = token['email']
     account.email_verified = token['email_verified']
@@ -66,7 +75,8 @@ def sign_out():
 
 @login_manager.user_loader
 def load_user(account_id):
-    return Account.query.get(account_id)
+    print(User.query.get(account_id))
+    return User.query.get(account_id)
 
 
 @login_manager.unauthorized_handler
